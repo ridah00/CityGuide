4fab581aac3ed3cf86409303b92b680a252e8d50
BarbierTheo <lebarbier.lh@gmail.com>
2024-12-17 11:30:38 +0100

    Ajout de bouton, responsive

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function parcourir_tout() {
 
       for (item of data["restaurants"]) {
         document.querySelector(".restaurant_tcontainer").innerHTML += `
-        <div class="customcard col-lg-4 col-10">
+        <div class="customcard col-lg-4 col-10" data-bs-toggle="modal" data-bs-target="#exemplenuméro${i}">
                 <img src="${item.image}" alt="photo-restau" class="imgcard">
                 <div class="row justify-content-between">
                       <div class="col-9">
@@ -49,16 +49,19 @@ function parcourir_tout() {
       }
       for (item of data["cultural"]) {
         document.querySelector(".sorties_tcontainer").innerHTML += `
-        <div class="customcard col-lg-4 col-10">
+      <div class="customcard col-lg-4 col-10" data-bs-toggle="modal" data-bs-target="#exemplenuméro${i}">
                 <img src="${item.image}" alt="photo-restau" class="imgcard">
                 <div class="row justify-content-between">
+                      <div class="col-9">
                     <h2 class="col-9 titlecard">
-                        "${item.name}"
+                        ${item.name}
                     </h2>
+
+                <p class="desccard">${item.address}</p>
+                    </div>
                     <button type="button" class="fork" data-bs-toggle="modal" data-bs-target="#exemplenuméro${i}"><i
                             class="fa-solid fa-utensils"></i></button>
                 </div>
-                <p class="desccard">${item.address}</p>
                 <div class="modal" tabindex="-1" id="exemplenuméro${i}">
                     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
                         <div class="modal-content">
@@ -67,6 +70,7 @@ function parcourir_tout() {
                                     aria-label="Close"></button>
                             </div>
                             <p>${item.description}</p>
+
                             <div class="d-flex flex-row-reverse mx-1">
                                 <a href="tel:+330000000" class="ctamodal"><i class="fa-solid fa-phone"></i></a>
                                 <a href="#" class="ctamodal"><i class="fa-solid fa-globe"></i></a>
@@ -81,24 +85,25 @@ function parcourir_tout() {
       }
       for (item of data["addresses"]) {
         document.querySelector(".adresses_tcontainer").innerHTML += `
-        <div class="customcard col-lg-4 col-10">
+        <div class="customcard col-lg-4 col-10" data-bs-toggle="modal" data-bs-target="#exemplenuméro${i}">
                 <img src="${item.image}" alt="photo-restau" class="imgcard">
                 <div class="row justify-content-between">
+                      <div class="col-9">
                     <h2 class="col-9 titlecard">
-                        "${item.name}"
+                        ${item.name}
                     </h2>
-                    <button type="button" class="fork" data-bs-toggle="modal"  data-bs-target="#exemplenuméro${i}"><i
+
+                <p class="desccard">${item.address}</p>
+                    </div>
+                    <button type="button" class="fork" data-bs-toggle="modal" data-bs-target="#exemplenuméro${i}"><i
                             class="fa-solid fa-utensils"></i></button>
                 </div>
-                <p class="desccard">${item.address}</p>
                 <div class="modal" tabindex="-1" id="exemplenuméro${i}">
                     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
                         <div class="modal-content">
-                        
                             <div class="d-flex flex-row-reverse m-2">
                                 <button type="button" class="btn-close" data-bs-dismiss="modal"
                                     aria-label="Close"></button>
-                                    
                             </div>
                             <p>${item.description}</p>
 
